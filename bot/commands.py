cb7d87a2cf896befe315370beb74d0c39e1d3345
kal902 <kaleab902@gmail.com>
2025-01-28 21:38:22 +0300

    added inline button for getting movie details, handler just returns a dummy

--- bot/commands.py
+++ bot/commands.py
@@ -1,5 +1,5 @@
-from telegram import Update
-from telegram.ext import ContextTypes
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import ContextTypes, CallbackContext
 import json
 
 from scraping.scraper import imdb_scraper
@@ -9,7 +9,7 @@ scraper = imdb_scraper()
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Welcome to the Movie & TV Show Tracker Bot! 🎥")
 
-async def search_movie(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def search_movie(update: Update, context: CallbackContext):
 
     movie_name = " ".join(context.args)  # Get user input after the command
     print(movie_name)
@@ -25,9 +25,19 @@ async def search_movie(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_photo(movie['img_url'])
         # then send other movie description
 
+        keyboard = [ [InlineKeyboardButton(f"detail for: {movie['title']}", callback_data=str(movie['detail_url']))] ]
         response = "Title: {title}\nrelease date: {release_date}\nstars: {stars}".format(title=movie['title'],
                                                                                             release_date=movie['release_date'],
                                                                                             stars=movie['stars'])
     
 
-        await update.message.reply_text(response)
+        await update.message.reply_text(response, reply_markup=InlineKeyboardMarkup(keyboard))
+
+async def movie_details(update: Update, context: CallbackContext):
+    print("detail command received")
+    query = update.callback_query
+    movie_detail_url = query.data
+
+    await query.answer()
+    await query.edit_message_text(text=f"{movie_detail_url}")
+
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from telegram.ext import Application, CommandHandler
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler
 from config import TELEGRAM_API_TOKEN
 
-from bot.commands import start, search_movie
+from bot.commands import start, search_movie, movie_details
 
 # Create Application and pass the Telegram API token
 application = Application.builder().token(TELEGRAM_API_TOKEN).build()
@@ -9,5 +9,6 @@ application = Application.builder().token(TELEGRAM_API_TOKEN).build()
 # Bot Command Handlers
 application.add_handler(CommandHandler("start", start))
 application.add_handler(CommandHandler("movie", search_movie))
+application.add_handler(CallbackQueryHandler(movie_details))
 # Start the bot
 application.run_polling()
